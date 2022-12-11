ecb9021e2a141ec02849e8d9f102e7af4baa14c5
yarkovaa <yarkova.av@edu.spbstu.ru>
2022-12-11 16:58:12 +0300

    9_1

--- js/form.js
+++ js/form.js
@@ -1,14 +1,23 @@
+import { scaleValue } from './scale.js';
+import { onUploadOverlayEffectChange, removeFilter } from './effects.js';
+
 function initForm() {
   const body = document.querySelector('body');
   const initialImgUpload = document.querySelector('.img-upload__start');
+  const form = document.querySelector('.img-upload__form');
   const editImg = document.querySelector('.img-upload__overlay');
+  const imgPreview = editImg.querySelector('.img-upload__preview');
   const uploadFile = document.querySelector('#upload-file');
   const imgDescr = document.querySelector('.text__description');
   const hashtags = document.querySelector('.text__hashtags');
+  const slider = form.querySelector('.effect-level__slider');
 
   const closeOption = () => {
-    editImg.classList.add('hidden');
     body.classList.remove('modal-open');
+    editImg.classList.add('hidden');
+    form.removeEventListener('change', onUploadOverlayEffectChange);
+    removeFilter();
+
     uploadFile.value = '';
     imgDescr.value = '';
     hashtags.value = '';
@@ -31,7 +40,13 @@ function initForm() {
 
   initialImgUpload.onchange = () => {
     editImg.classList.remove('hidden');
+    slider.classList.add('hidden');
     body.classList.add('modal-open');
+
+    scaleValue.value = '100%';
+    imgPreview.style = `transform: scale(${scaleValue})`;
+
+    form.addEventListener('change', onUploadOverlayEffectChange);
 
     closeButton();
   };
